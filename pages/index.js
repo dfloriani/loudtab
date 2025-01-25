4d8706a34b1468817156d6088a7914807ad217e0
Dayane Floriani Antunes <36275146+dfloriani@users.noreply.github.com>
2025-01-25 14:28:55 +0000

    Prettier config

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-	return <h1>All we have to decide is what to do with the time that is given to us.</h1>
+  return (
+    <h1>
+      All we have to decide is what to do with the time that is given to us.
+    </h1>
+  );
 }
 
 export default Home;
