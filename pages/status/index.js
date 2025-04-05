97ce63f5c720d3db65c28262e86c7c7bbb408032
Dayane Floriani Antunes <36275146+dfloriani@users.noreply.github.com>
2025-04-05 17:34:24 +0000

    feat: status page

---
+++ pages/status/index.js
@@ -0,0 +1,60 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status Page</h1>
+      <UpdatedAt />
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updatetAtText = "Loading...";
+
+  if (!isLoading && data) {
+    updatetAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Last update: {updatetAtText}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInformation = "Loading...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Version: {data.dependencies.database.version}</div>
+        <div>
+          Opened connections: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Maximum connections: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
+  );
+}
