3043c386c637cc3585861d14804038ac49a0ce11
Dayane Floriani Antunes <36275146+dfloriani@users.noreply.github.com>
2025-05-03 16:50:11 +0000

    feat: hash `password` in `user.create`

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -34,7 +34,7 @@ describe("GET /api/v1/users/[username]", () => {
         id: response2Body.id,
         username: "SameCase",
         email: "samecase@gmail.com",
-        password: "pass123456",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
@@ -69,7 +69,7 @@ describe("GET /api/v1/users/[username]", () => {
         id: response2Body.id,
         username: "DifferentCase",
         email: "differentcase@gmail.com",
-        password: "pass123456",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
